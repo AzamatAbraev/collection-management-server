8bf2743a2be17229df79e2c65706d48e6f112f5f
Your Name <azamatabraev07@gmail.com>
2024-03-21 14:44:37 +0500

    image upload test on server

--- gcs.js
+++ gcs.js
@@ -6,7 +6,7 @@ const path = require("path");
 const multerUpload = multer({ storage: multer.memoryStorage() });
 
 const storage = new Storage({
-  keyFilename: process.env.GCS_KEYFILE_PATH,
+  keyFilename: path.join(__dirname, process.env.GCS_KEYFILE_PATH),
   projectId: process.env.GCS_PROJECT_ID,
 });
 const bucket = storage.bucket(process.env.GCS_BUCKET_NAME);
